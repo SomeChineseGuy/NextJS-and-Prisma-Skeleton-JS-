d6fda6a9d103ad627798aa28e578a9e11918a55c
Alvin Ng <alvin.cl.ng@gmail.com>
2023-04-04 13:11:58 -0700

    Next install with basic values

---
+++ skeleton/next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['http.cat', 'example.com'],
+  },
+}
+
+module.exports = nextConfig
