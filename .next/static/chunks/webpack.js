6e16235442d65dc714d751ccc8edb5a98c15fe8a
davique0 <davique@gmail.com>
2023-05-07 18:30:52 -0400

    main commit

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "3ad83d3a168dea17"; }
+/******/ 		__webpack_require__.h = function() { return "90a23f80bd1e0241"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
