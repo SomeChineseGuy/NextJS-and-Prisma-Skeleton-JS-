38feba35fdbe2cde74f529ec7e448f9af8b663d4
davique0 <davique@gmail.com>
2023-05-07 07:26:35 -0400

    Add Auth0

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9f70afb341ca9b59"; }
+/******/ 		__webpack_require__.h = function() { return "ffa534a8a8daf952"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
