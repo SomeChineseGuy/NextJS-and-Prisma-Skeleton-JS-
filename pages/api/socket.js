dbfbe8ac439355d4d3b1632dcc7a85cdd23b630c
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-05-09 16:43:38 -0600

    Socket io setup

--- pages/api/socket.js
+++ pages/api/socket.js
@@ -1,32 +1,33 @@
-import {Server} from "socket.io"
+import { Server } from "socket.io"
 
-export default function SocketHandler (req, res){
-  if(res.socket.server.io){
-    console.log("Already connected");
-    res.end()
+export default function SocketHandler(req, res) {
+  if (res.socket.server.io) {
+    console.log("Socket is already running");
+    res.end();
     return;
-} else {
-const io = new Server(res.socket.server);
-res.socket.server.io = io;
+  }
+  console.log("Socket is initializing")
+  const io = new Server(res.socket.server);
+  res.socket.server.io = io;
 
-io.on("connection", socket => {
-  console.log("User Connected:", socket.id);
+  io.on("connection", (socket) => {
+    console.log("User Connected:", socket.id);
 
-  socket.on("join_room", (data) => {
-    socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    socket.on("send", (data) => {
+      console.log("Backend data", data)
+      socket.to(data.room).emit("receive", data)
+    })
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
+      console.log(`User with ID: ${socket.id} joined room: ${data}`)
+    })
+
+    socket.on("disconnect", () => {
+      console.log("User Disconnected", socket.id)
+    })
   })
-
-  socket.on("send-message", (data) => {
-    socket.to(data.room).emit("receive_message", data)
-   })
-
-  socket.on("disconnect", () => {
-    console.log("User Disconnected", socket.id)
-  })
-})
-}
-res.end()
+  res.end()
 }
 
 
