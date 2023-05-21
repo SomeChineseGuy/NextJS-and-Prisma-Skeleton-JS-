deca48ce4d79302536be630baf79df60d8fa73ce
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-05-21 09:01:17 -0600

    Add usercontext and pass props

--- pages/_app.js
+++ pages/_app.js
@@ -2,13 +2,28 @@ import "@/styles/globals.css";
 import "../styles/tailwind.css";
 import Layout from "./components/Layout";
 import { UserProvider } from "@auth0/nextjs-auth0/client";
+import UserContext from "@/context/userInfoContext";
+import { PrismaClient } from "@prisma/client";
 
-export default function App({ Component, pageProps}) {
+
+export default function App({ Component, pageProps, users}) {
+  console.log(users)
   return (
     <UserProvider>
+      <UserContext.Provider value={pageProps}>
       <Layout users={pageProps}>
         <Component {...pageProps} />
       </Layout>
+      </UserContext.Provider>
     </UserProvider>
   );
+}
+
+
+export async function getStaticProps() {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany()
+  return {
+    props: {users}
+  };
 }
