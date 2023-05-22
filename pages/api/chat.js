d9ca6f88cba9ac7a38525c1fd04bf2531fc97c2b
Brooke Nagy <brooke_nagy123@hotmail.com>
2023-05-21 19:27:12 -0600

    Remove console logs

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -2,9 +2,7 @@ import { PrismaClient } from "@prisma/client";
 
 export default async function handler(req, res) {
   const { dataActiveChat, dataSender, dataMessage } = req.body;
-  console.log(dataActiveChat);
   const id = parseInt(dataActiveChat);
-  console.log(id);
   const prisma = new PrismaClient();
   const message = await prisma.message.create({
     data: {
